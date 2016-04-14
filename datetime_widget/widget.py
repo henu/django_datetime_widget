bba04fe734335f1949ac7729bc9daa1192f54523
Henrik Heino <henrik.heino@gmail.com>
2016-04-14 22:56:33 +0300

    DatetimeWidget can now convert text format to datetime object and back.

--- datetime_widget/widget.py
+++ datetime_widget/widget.py
@@ -2,9 +2,38 @@ from django.forms.utils import flatatt
 from django.forms.widgets import Widget
 from django.utils.html import format_html
 
+from datetime import datetime
+
 
 class DatetimeWidget(Widget):
 
+    def __init__(self, *args, **kwargs):
+        self.datetime_format = kwargs.pop('format', '%c')
+        super(DatetimeWidget, self).__init__(*args, **kwargs)
+
     def render(self, name, value, attrs=None):
-        final_attrs = self.build_attrs(attrs, type="text", name=name)
-        return format_html('<input{} disabled />', flatatt(final_attrs))
+        if not value:
+            value = ''
+        if isinstance(value, datetime):
+            value = value.strftime(self.datetime_format)
+
+        final_attrs = self.build_attrs(
+            attrs,
+            type="text",
+            name=name,
+            value=value,
+        )
+        return format_html('<input{} readonly />', flatatt(final_attrs))
+
+    def value_from_datadict(self, data, files, name):
+        value = data.get(name, None)
+
+        # Convert text value to datetime object.
+        # If conversion fails, then string data is suitable. It gives error and can be edited by user.
+        if value:
+            try:
+                value = datetime.strptime(value, self.datetime_format)
+            except ValueError:
+                pass
+
+        return value
